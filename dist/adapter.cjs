0f7c435c0d76bd2860bc0b58db5d09bb505d63b0
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2024-02-13 19:44:32 +0530

    chore: update build method and underlying package

--- dist/adapter.cjs
+++ dist/adapter.cjs
@@ -1,3 +1,21 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 var __async = (__this, __arguments, generator) => {
   return new Promise((resolve, reject) => {
     var fulfilled = (value) => {
@@ -20,26 +38,18 @@ var __async = (__this, __arguments, generator) => {
 };
 
 // src/adapter.ts
-import {
-  BaseMessageSignerWalletAdapter,
-  WalletConnectionError,
-  WalletDisconnectionError,
-  WalletNotConnectedError,
-  WalletPublicKeyError,
-  WalletReadyState,
-  WalletSendTransactionError,
-  WalletSignMessageError,
-  isVersionedTransaction,
-  WalletSignTransactionError
-} from "@solana/wallet-adapter-base";
-import {
-  Transaction,
-  VersionedTransaction
-} from "@solana/web3.js";
-import { PublicKey } from "@solana/web3.js";
-import { MoonGateEmbed } from "@anishde12020/moongate-solana-wallet-sdk";
+var adapter_exports = {};
+__export(adapter_exports, {
+  MoongateWalletAdapter: () => MoongateWalletAdapter,
+  MoongateWalletName: () => MoongateWalletName
+});
+module.exports = __toCommonJS(adapter_exports);
+var import_wallet_adapter_base = require("@solana/wallet-adapter-base");
+var import_web3 = require("@solana/web3.js");
+var import_web32 = require("@solana/web3.js");
+var import_moongate_solana_wallet_sdk = require("@anishde12020/moongate-solana-wallet-sdk");
 var MoongateWalletName = "Ethereum Wallet";
-var MoongateWalletAdapter = class extends BaseMessageSignerWalletAdapter {
+var MoongateWalletAdapter = class extends import_wallet_adapter_base.BaseMessageSignerWalletAdapter {
   constructor(config) {
     super();
     this.name = MoongateWalletName;
@@ -47,7 +57,7 @@ var MoongateWalletAdapter = class extends BaseMessageSignerWalletAdapter {
     this.icon = "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48IS0tIFVwbG9hZGVkIHRvOiBTVkcgUmVwbywgd3d3LnN2Z3JlcG8uY29tLCBHZW5lcmF0b3I6IFNWRyBSZXBvIE1peGVyIFRvb2xzIC0tPgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIKYXJpYS1sYWJlbD0iRXRoZXJldW0iIHJvbGU9ImltZyIKdmlld0JveD0iMCAwIDUxMiA1MTIiPjxyZWN0CndpZHRoPSI1MTIiIGhlaWdodD0iNTEyIgpyeD0iMTUlIgpmaWxsPSIjZmZmZmZmIi8+PHBhdGgKZmlsbD0iIzNDM0MzQiIgZD0ibTI1NiAzNjJ2MTA3bDEzMS0xODV6Ii8+PHBhdGgKZmlsbD0iIzM0MzQzNCIgZD0ibTI1NiA0MWwxMzEgMjE4LTEzMSA3OC0xMzItNzgiLz48cGF0aApmaWxsPSIjOEM4QzhDIiBkPSJtMjU2IDQxdjE1OGwtMTMyIDYwbTAgMjVsMTMyIDc4djEwNyIvPjxwYXRoCmZpbGw9IiMxNDE0MTQiIGQ9Im0yNTYgMTk5djEzOGwxMzEtNzgiLz48cGF0aApmaWxsPSIjMzkzOTM5IiBkPSJtMTI0IDI1OWwxMzItNjB2MTM4Ii8+PC9zdmc+";
     this.supportedTransactionVersions = /* @__PURE__ */ new Set(["legacy", 0]);
     this._position = "top-right";
-    this._readyState = typeof window === "undefined" || typeof document === "undefined" ? WalletReadyState.Unsupported : WalletReadyState.Installed;
+    this._readyState = typeof window === "undefined" || typeof document === "undefined" ? import_wallet_adapter_base.WalletReadyState.Unsupported : import_wallet_adapter_base.WalletReadyState.Installed;
     this._connecting = false;
     this._wallet = null;
     this._publicKey = null;
@@ -73,11 +83,11 @@ var MoongateWalletAdapter = class extends BaseMessageSignerWalletAdapter {
       if (this.connected || this.connecting)
         return;
       if (this._wallet) {
-        throw new WalletConnectionError("Already connected");
+        throw new import_wallet_adapter_base.WalletConnectionError("Already connected");
       }
       this._connecting = true;
       try {
-        this._wallet = new MoonGateEmbed();
+        this._wallet = new import_moongate_solana_wallet_sdk.MoonGateEmbed();
         const publicKeyData = yield this._wallet.sendCommand(
           "login",
           {
@@ -85,7 +95,7 @@ var MoongateWalletAdapter = class extends BaseMessageSignerWalletAdapter {
           }
         );
         if (publicKeyData) {
-          let publicKey = new PublicKey(publicKeyData);
+          let publicKey = new import_web32.PublicKey(publicKeyData);
           this._publicKey = publicKey;
           this.emit("connect", publicKey);
           if (this == null ? void 0 : this._position) {
@@ -94,11 +104,11 @@ var MoongateWalletAdapter = class extends BaseMessageSignerWalletAdapter {
             this._wallet.moveModal();
           }
         } else {
-          throw new WalletPublicKeyError("No response from MoonGate wallet.");
+          throw new import_wallet_adapter_base.WalletPublicKeyError("No response from MoonGate wallet.");
         }
       } catch (error) {
         console.error("Error encountered during connection:", error);
-        throw new WalletConnectionError(error.message);
+        throw new import_wallet_adapter_base.WalletConnectionError(error.message);
       } finally {
         this._connecting = false;
         console.log("Connected:", (_a = this._publicKey) == null ? void 0 : _a.toString());
@@ -115,7 +125,7 @@ var MoongateWalletAdapter = class extends BaseMessageSignerWalletAdapter {
         this.emit("disconnect");
       } catch (error) {
         console.error("Error encountered during disconnection:", error);
-        throw new WalletDisconnectionError(error.message);
+        throw new import_wallet_adapter_base.WalletDisconnectionError(error.message);
       }
     });
   }
@@ -123,10 +133,10 @@ var MoongateWalletAdapter = class extends BaseMessageSignerWalletAdapter {
     return __async(this, arguments, function* (transaction, connection, options = {}) {
       let signature;
       if (!this._wallet) {
-        throw new WalletNotConnectedError();
+        throw new import_wallet_adapter_base.WalletNotConnectedError();
       }
       try {
-        if (!isVersionedTransaction(transaction)) {
+        if (!(0, import_wallet_adapter_base.isVersionedTransaction)(transaction)) {
           transaction = yield this.prepareTransaction(
             transaction,
             connection,
@@ -141,16 +151,16 @@ var MoongateWalletAdapter = class extends BaseMessageSignerWalletAdapter {
         return signature;
       } catch (error) {
         console.error("Error encountered during transaction submission:", error);
-        throw new WalletSendTransactionError(error.message);
+        throw new import_wallet_adapter_base.WalletSendTransactionError(error.message);
       }
     });
   }
   signTransaction(transaction) {
     return __async(this, null, function* () {
       if (!this._wallet) {
-        throw new WalletNotConnectedError();
+        throw new import_wallet_adapter_base.WalletNotConnectedError();
       }
-      if (isVersionedTransaction(transaction)) {
+      if ((0, import_wallet_adapter_base.isVersionedTransaction)(transaction)) {
         const data = transaction.serialize();
         try {
           const signedTransaction = yield this._wallet.sendCommand(
@@ -161,13 +171,13 @@ var MoongateWalletAdapter = class extends BaseMessageSignerWalletAdapter {
               isVersionedTransaction: true
             }
           );
-          const finalTransaction = VersionedTransaction.deserialize(
+          const finalTransaction = import_web3.VersionedTransaction.deserialize(
             signedTransaction
           );
           return finalTransaction;
         } catch (error) {
           console.error("Error encountered during transaction signing:", error);
-          throw new WalletSignTransactionError(error.message);
+          throw new import_wallet_adapter_base.WalletSignTransactionError(error.message);
         }
       } else {
         try {
@@ -180,13 +190,13 @@ var MoongateWalletAdapter = class extends BaseMessageSignerWalletAdapter {
               isVersionedTransaction: false
             }
           );
-          const finalTransaction = Transaction.from(
+          const finalTransaction = import_web3.Transaction.from(
             Uint8Array.from(signedTransaction)
           );
           return finalTransaction;
         } catch (error) {
           console.error("Error encountered during transaction signing:", error);
-          throw new WalletSignTransactionError(error.message);
+          throw new import_wallet_adapter_base.WalletSignTransactionError(error.message);
         }
       }
     });
@@ -203,7 +213,7 @@ var MoongateWalletAdapter = class extends BaseMessageSignerWalletAdapter {
   signMessage(message) {
     return __async(this, null, function* () {
       if (!this._wallet) {
-        throw new WalletNotConnectedError();
+        throw new import_wallet_adapter_base.WalletNotConnectedError();
       }
       try {
         const signedMessage = yield this._wallet.sendCommand(
@@ -217,12 +227,13 @@ var MoongateWalletAdapter = class extends BaseMessageSignerWalletAdapter {
         return Uint8ArraySignedMessage;
       } catch (error) {
         console.error("Error encountered during message signature:", error);
-        throw new WalletSignMessageError(error.message);
+        throw new import_wallet_adapter_base.WalletSignMessageError(error.message);
       }
     });
   }
 };
-export {
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
   MoongateWalletAdapter,
   MoongateWalletName
-};
+});
---
+++ dist/adapter.d.ts
@@ -0,0 +1,30 @@
+import { WalletName, BaseMessageSignerWalletAdapter, WalletReadyState, SendTransactionOptions } from '@solana/wallet-adapter-base';
+import { TransactionVersion, PublicKey, Transaction, VersionedTransaction, Connection, TransactionSignature } from '@solana/web3.js';
+
+declare const MoongateWalletName: WalletName<"MoonGate">;
+declare class MoongateWalletAdapter extends BaseMessageSignerWalletAdapter {
+    name: WalletName<"MoonGate">;
+    url: string;
+    icon: string;
+    readonly supportedTransactionVersions: ReadonlySet<TransactionVersion>;
+    private _connecting;
+    private _wallet;
+    private _position;
+    private _publicKey;
+    private _readyState;
+    constructor(config?: {
+        position?: string;
+    });
+    get publicKey(): PublicKey | null;
+    get connecting(): boolean;
+    get connected(): boolean;
+    get readyState(): WalletReadyState;
+    connect(): Promise<void>;
+    disconnect(): Promise<void>;
+    sendTransaction<T extends Transaction | VersionedTransaction>(transaction: T, connection: Connection, options?: SendTransactionOptions): Promise<TransactionSignature>;
+    signTransaction<T extends Transaction | VersionedTransaction>(transaction: T): Promise<T>;
+    signAllTransactions<T extends Transaction | VersionedTransaction>(transactions: T[]): Promise<T[]>;
+    signMessage(message: Uint8Array): Promise<Uint8Array>;
+}
+
+export { MoongateWalletAdapter, MoongateWalletName };
